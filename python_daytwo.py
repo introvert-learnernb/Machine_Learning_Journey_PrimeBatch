bbf0cade5ec385958f4d611442114b41c22eacf6
NB <introvertlearnernb@gmail.com>
2025-11-18 14:16:46 +0545

    third day

--- python_daytwo.py
+++ python_daytwo.py
@@ -49,10 +49,58 @@
 #loops  
 
 
-i = 1
+# i = 1
 
-while i <= 10:
-    print("Hello, World!", i)
-    i += 1
+# while i <= 10:
+#     print("Hello, World!", i)
+#     i += 1
     
-print("After loop , i =", i)
+# print("After loop , i =", i)
+
+
+#multiplication table for any number n
+
+# n = int(input("Enter a number to generate its multiplication table: "))
+
+# i = 1
+# while i <= 10:
+#     print(n, "x", i, "=", n * i)
+#     i += 1
+# print("Multiplication table for", n, "completed.")
+
+#break and continue
+
+#break exits from loop
+#continue skips the current iteration and moves to the next one
+
+# for i in range(1, 11):
+#     if i == 5:
+#         print("Skipping number 5")
+#         continue
+#     if i == 8:
+#         print("Stopping the loop at number 8")
+#         break
+#     print("Current number:", i)
+    
+#for loop
+#range function - range(start, stop, step)  - range(n) 0 to n-1 - range(1,n) 1 to n-1 - range(1,n,2) 1 to n-1 with step 2
+
+
+# for i in range(1, 11):
+#     print("Hello, World!", i)
+    
+#counting number of i's in Application word
+
+# count = 0
+# for char in "Application":
+#     if char == 'i':
+#         count += 1
+# print("Number of 'i's in 'Application':", count)
+
+#counting number of vowels in a string
+
+count = 0
+for char in "Education":
+    if char.lower() in 'aeiou':
+        count += 1
+print("Number of vowels in 'Education':", count)
